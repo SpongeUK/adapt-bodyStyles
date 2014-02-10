21bf6a640c9adf4686a0f7c8ece26de2a4ae3e36
Chris Jones <chris.jones@spongeuk.com>
2014-02-10 14:15:39 +0000

    IE7/8 support

--- js/jquery-plugins.js
+++ js/jquery-plugins.js
@@ -1,11 +1,11 @@
-define(["jquery"], function ($) {
+define(["jquery", "underscore"], function ($, _) {
     $.fn.removeAllClasses = function (options) {
         options = $.extend({
             match: /./gi
         }, options);
 
         var findClasses = (options.match.constructor === RegExp) ? options.match.test.bind(options.match) : (typeof options.match !== 'function') ? function identity(item) { return item === options.match; } : options.match,
-            classes = (this.attr('class') || '').split(' ').filter(findClasses).join(' ');
+            classes = _.filter((this.attr('class') || '').split(' '), findClasses).join(' ');
 
         this.removeClass(classes);
 
